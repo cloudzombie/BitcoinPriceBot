ea1ffa172494ca461db9f1ae26e4e14d20814aa3
nishilshah17 <nishil.shah17@gmail.com>
2016-01-08 17:15:50 -0500

    sends current btc price to all friends

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var login = require("facebook-chat-api");
-
+var btc = require("btcaverage");
 var http = require("http");
 var a_sync = require("async");
-
 var config = require("./config.json");
+
+var fb_api;
+var average;
+var message;
 
 //prevent server from sleeping
 http.createServer(function (req, res) {
@@ -17,3 +20,23 @@ setInterval(function() {
     console.log("pong");
   });
 }, 300000);
+
+btc().then(function(priceDetails){
+  average = parseFloat(priceDetails.average).toFixed(2);
+  message = {body: "1 BTC = $"+average};
+});
+
+login({email: config.username, password: config.password}, function callback (err, api) {
+  if(err) return console.error(err);
+
+  fb_api = api;
+
+  api.getFriendsList(function(err, data) {
+    if(err) return console.error(err);
+
+    for(var i = 0; i < data.length; i++) {
+      var userID = data[i].userID;
+      api.sendMessage(message, userID);
+    }
+  });
+});
